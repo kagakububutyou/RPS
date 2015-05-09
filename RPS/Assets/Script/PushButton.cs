c6f531a41244c457db6a2d8fd91aba07c8a66764
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-05-09 13:55:10 +0900

    ボタンの判定

---
+++ RPS/Assets/Script/PushButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PushButton : MonoBehaviour {
+
+    [SerializeField]
+    Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        button.onClick.AddListener(ShowLog);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ShowLog()
+    {
+        Debug.Log(button.name);
+    }
+}
