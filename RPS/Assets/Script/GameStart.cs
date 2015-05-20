90b9c56c3d17dc24e8502d433635847eb16cdd19
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-05-21 06:52:56 +0900

    タイトル追加

---
+++ RPS/Assets/Script/GameStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour {
+
+    [SerializeField]
+    private Button button = null;	    //  ボタンの取得
+
+    /// <summary>
+    /// 初期化のためにこれを使用してください
+    /// </summary>
+	private void Start () 
+    {
+        button.onClick.AddListener(PushButton);                    //  ボタンが押された時(イベントコールバック)
+	}
+    /// <summary>
+    /// ボタンが押された時
+    /// </summary>
+    private void PushButton()
+    {
+        Application.LoadLevel("main");
+    }
+}
