440f60d981dfc34fa1276e25368bc998925c3064
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-06-08 21:33:21 +0900

    タッチした場所に円を出した

---
+++ RPS/Assets/Script/CreateCircle.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreateCircle : MonoBehaviour {
+
+    [SerializeField]
+    private Image image = null;
+    [SerializeField]
+    private float countMax = 0.0f;
+
+    [SerializeField]
+    private Color32 color = new Color32(0, 0, 0, 0);
+
+    private float fadeAlpha = 1.0f;
+
+	// Use this for initialization
+	private void Start () 
+    {
+        // タッチしたときのスクリーン座標を取得
+        Vector2 screenPos = Input.mousePosition;
+        image.transform.position = screenPos;
+        StartCoroutine(CountDown(countMax));
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        DrawCircle();
+	}
+
+    private IEnumerator CountDown(float interval)
+    {
+        float time = 0;
+        while (time <= interval)
+        {
+            fadeAlpha = Mathf.Lerp(1.0f, 0.0f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+            Debug.Log(fadeAlpha);
+        }
+    }
+
+    private void DrawCircle()
+    {
+        image.color = new Vector4(color.r, color.g, color.b, fadeAlpha);
+        if(fadeAlpha < 0.1)
+        {
+            image.enabled = false;
+        }
+    }
+}
---
+++ RPS/Assets/Script/TouchScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/// <summary>
+/// 画面タッチ
+/// </summary>
+public class TouchScreen : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas image = null;
+
+	// Use this for initialization
+	private void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+    {
+        TouchPos();
+
+        //Destroy(this, 0.5f);
+	}
+
+    private void TouchPos()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(image);
+        }
+    }
+}
