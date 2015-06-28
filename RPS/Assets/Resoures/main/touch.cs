14939765e9c699ae7a09eef97a0a3867f591ef13
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-06-28 20:39:37 +0900

    背景の画像をとりあえず付けた

---
+++ RPS/Assets/Resoures/main/touch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touch: MonoBehaviour
+{
+    public GameObject sprite = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Instantiate(sprite, new Vector3(10, 0, 0), Quaternion.identity);
+
+        }
+	}
+}
